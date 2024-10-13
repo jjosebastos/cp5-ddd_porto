f948dfb9e97ab827b9be1956c48be777a06f1d57
Jose Bezerra <josebezerrabastos@gmail.com>
2024-10-13 20:47:12 -0300

    estrutura do projeto + entities

---
+++ DDD-MotorInsight/src/main/java/org/motins/entity/Cliente.java
@@ -0,0 +1,47 @@
+package org.motins.entity;
+
+public class Cliente {
+
+    private int idCliente;
+    private String tipoCliente;
+    private String statusCliente;
+
+    public Cliente(int idCliente, String tipoCliente, String statusCliente) {
+        this.idCliente = idCliente;
+        setTipoCliente(tipoCliente);
+        this.statusCliente = statusCliente;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente.toUpperCase();
+    }
+
+    public String getStatusCliente() {
+        return statusCliente;
+    }
+
+    public void setStatusCliente(String statusCliente) {
+        this.statusCliente = statusCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", tipoCliente='" + tipoCliente + '\'' +
+                ", statusCliente='" + statusCliente + '\'' +
+                '}';
+    }
+}
---
+++ DDD-MotorInsight/src/main/java/org/motins/entity/Endereco.java
@@ -0,0 +1,143 @@
+package org.motins.entity;
+
+public class Endereco {
+
+    private int idEndereco;
+    private String rua;
+    private String numeroEndereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String complemento;
+    private String idCliente;
+    private String idAutorizada;
+    private String idSeguradora;
+
+    public Endereco(){}
+    public Endereco(int idEndereco, String rua, String numeroEndereco ,String bairro, String cidade, String uf,
+                    String cep, String complemento, String idCliente, String idAutorizada, String idSeguradora) {
+        this.idEndereco = idEndereco;
+        this.rua = rua;
+        this.numeroEndereco = numeroEndereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        setCep(cep);
+        this.complemento = complemento;
+        this.idCliente = idCliente;
+        this.idAutorizada = idAutorizada;
+        this.idSeguradora = idSeguradora;
+
+    }
+
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumeroEndereco() {
+        return numeroEndereco;
+    }
+
+    public void setNumeroEndereco(String numeroEndereco) {
+        this.numeroEndereco = numeroEndereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Boolean isCepValido(String cep) {
+        return cep.matches("\\d{5}-\\d{3}");
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getIdAutorizada() {
+        return idAutorizada;
+    }
+
+    public void setIdAutorizada(String idAutorizada) {
+        this.idAutorizada = idAutorizada;
+    }
+
+    public String getIdSeguradora() {
+        return idSeguradora;
+    }
+
+    public void setIdSeguradora(String idSeguradora) {
+        this.idSeguradora = idSeguradora;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "idEndereco=" + idEndereco +
+                ", rua='" + rua + '\'' +
+                ", numeroEndereco='" + numeroEndereco + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", uf='" + uf + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", idCliente=" + idCliente +
+                ", idAutorizada=" + idAutorizada +
+                ", idSeguradora=" + idSeguradora +
+                '}';
+    }
+}
